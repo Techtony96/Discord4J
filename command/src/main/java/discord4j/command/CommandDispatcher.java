7d5c7bc6ecae9c164dd057b925cc6290db0c3ba9
danthonywalker <thedanthonywalker@gmail.com>
2018-08-11 07:52:09 -0600

    Incorporated ProviderContext, removed CommandException, allowed multiple commands to be executed, default dispatcher now recovers errors, added copyright, made some naming more consistent.

--- command/src/main/java/discord4j/command/CommandDispatcher.java
+++ command/src/main/java/discord4j/command/CommandDispatcher.java
@@ -1,7 +1,23 @@
+/*
+ * This file is part of Discord4J.
+ *
+ * Discord4J is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Discord4J is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Discord4J.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package discord4j.command;
 
 import discord4j.core.event.domain.message.MessageCreateEvent;
-import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
 
 import java.util.Set;
 
@@ -15,13 +31,13 @@ import java.util.Set;
 public interface CommandDispatcher {
 
     /**
-     * Called to handle the logic of invoking a command based on event context.
+     * Called to handle the logic of invoking a command based on an event.
      *
-     * @param event The event context.
-     * @param providers The {@link discord4j.command.CommandProvider}s this dispatcher should consider for dispatching.
+     * @param event The event for command execution.
+     * @param providers The {@link CommandProvider}s this dispatcher should consider for dispatching.
      * @param errorHandler The error handler to pass to command.
-     * @return A mono which emits executed command.
+     * @return A flux which emits executed commands.
      */
-    Mono<? extends Command> dispatch(MessageCreateEvent event, Set<CommandProvider> providers,
+    Flux<? extends Command> dispatch(MessageCreateEvent event, Set<CommandProvider> providers,
                                      CommandErrorHandler errorHandler);
 }
--- command/src/main/java/discord4j/command/CommandProvider.java
+++ command/src/main/java/discord4j/command/CommandProvider.java
@@ -1,11 +1,25 @@
+/*
+ * This file is part of Discord4J.
+ *
+ * Discord4J is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Discord4J is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Discord4J.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package discord4j.command;
 
 import discord4j.core.event.domain.message.MessageCreateEvent;
-import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
 
-/**
- * This represents a generic command provider.
- */
+/** This represents a generic command provider. */
 @FunctionalInterface
 public interface CommandProvider {
 
@@ -21,8 +35,8 @@ public interface CommandProvider {
      * @param commandEndIndex The end index (exclusive) of where naive parsing of arguments should occur.
      * @return The matched command based on the context, or empty if no command matched.
      */
-    Mono<? extends Command> provide(MessageCreateEvent context,
-                                    String commandName,
-                                    int commandStartIndex,
-                                    int commandEndIndex);
+    Flux<ProviderContext> provide(MessageCreateEvent context,
+                                  String commandName,
+                                  int commandStartIndex,
+                                  int commandEndIndex);
 }
